b3532541cff3d546f44b360e663da124499fd0f4
Mming <752556473@qq.com>
2024-02-27 18:09:24 +0800

    使用git提交的第一个文件

---
+++ first.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+@Project: Git_Study_Project 
+@File   : first.py
+@IDE    : PyCharm
+@Author : Mming
+@Time   : 2024/2/27 18:07
+@Description:
+"""
+import numpy as np
+import pnadas as pd
