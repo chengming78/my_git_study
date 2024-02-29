a9e38ce301d16905ff529fba190e844b6dab8acf
Mming <752556473@qq.com>
2024-02-29 23:31:55 +0800

    ming修复的bug

---
+++ thrid.1.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+@Project: Git_Study_Project 
+@File   : thrid.1.py
+@IDE    : PyCharm
+@Author : Mming
+@Time   : 2024/2/29 23:29
+@Description:
+"""
+import numpy as np
+import pandas as pd
+
+txt = """
+      增加了3.1的python功能, 修改了一个bug
+      """
