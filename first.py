d1d9f5836fa314811e06b554b7388d7f2387ce0f
Mming <752556473@qq.com>
2024-02-29 15:18:26 +0800

    测试LLM所需的pytorch环境

--- first.py
+++ first.py
@@ -5,7 +5,14 @@
 @IDE    : PyCharm
 @Author : Mming
 @Time   : 2024/2/27 18:07
-@Description:
+@Description: 测试pytorch环境
 """
 import numpy as np
 import pandas as pd
+import torch
+
+
+print(torch.__version__)
+print(torch.version.cuda)
+print(torch.backends.cudnn.version())
+print(torch.cuda.is_available())
