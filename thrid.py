7897b8aa2c60574226e9450b01e549bd00c6d220
Mming <752556473@qq.com>
2024-02-29 22:33:52 +0800

    第一次提交

---
+++ thrid.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+@Project: Git_Study_Project 
+@File   : thrid.py
+@IDE    : PyCharm
+@Author : Mming
+@Time   : 2024/2/29 22:31
+@Description:
+"""
+import numpy as np
+import pandas as pd
+
+
+print("第一次提交")
